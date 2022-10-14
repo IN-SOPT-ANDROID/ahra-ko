fd43cfa4df0833062dc084a73225d7aff1d4453c
ahra_ <79412889+ahra1221@users.noreply.github.com>
2022-10-14 20:18:54 +0900

    Update MainActivity.kt

--- app/src/main/java/org/sopt/sample/MainActivity.kt
+++ app/src/main/java/org/sopt/sample/MainActivity.kt
@@ -1,10 +1,22 @@
-package org.sopt.sample
+package com.sopt.week2
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.sopt.week2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.main_container)
+        if(currentFragment == null){
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_container, HomeFragment())
+                .commit()
+        }
     }
 }

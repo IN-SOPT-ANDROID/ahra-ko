48b3fa7a8f7684c493b55ced8a93440c94df4a8a
murjune <sis9221302@gmail.com>
2022-11-18 16:55:50 +0900

    Merge conflict resolve

--- app/src/main/java/org/sopt/sample/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/SignUpActivity.kt
@@ -1,21 +1,20 @@
-package com.sopt.week1
+package org.sopt.sample
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
-import com.sopt.week1.databinding.ActivitySignUpBinding
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
-    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnSign.setOnClickListener {
-            if (binding.edtSignupId.length() in 6..10 && binding.edtSignupPw.length() in 8..12)  {
+            if (binding.edtSignupId.length() in 6..10 && binding.edtSignupPw.length() in 8..12) {
                 val intent = Intent(this@SignUpActivity, MainActivity::class.java)
 
                 intent.putExtra("userId", binding.edtSignupId.text.toString())

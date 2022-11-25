4f133523adefbf6e9c7d304ecdff55bcb3ca1bf8
ahra_ <lizze011221@gmail.com>
2022-11-25 17:49:57 +0900

    [feat] id,pw,name 조건확인 기능 구현 성공 !!!!!

--- app/src/main/java/org/sopt/sample/SignUp/view/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/SignUp/view/SignUpActivity.kt
@@ -5,19 +5,18 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.sample.SignIn.view.SignInActivity
+import org.sopt.sample.SignUp.viewmodel.SignUpViewModel
 import org.sopt.sample.databinding.ActivitySignUpBinding
-import org.sopt.sample.remote.api.RequestSignUp
-import org.sopt.sample.remote.api.ResponseSignUp
-import org.sopt.sample.remote.api.ServicePool
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
-    private var signupService = ServicePool.signUpService
+    private val viewModel by viewModels<SignUpViewModel>()
+    private var idFlag = false
+    private var pwFlag = false
+    private var nameFlag = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,77 +24,83 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSign.isEnabled = false
-        btnActivated()
+        textWatcher()
         clickEvent()
     }
 
-    private fun isEmpty() : Boolean{
-        with(binding){
-            if (!edtSignupEmail.text.toString().isBlank() &&
-                !edtSignupPw.text.toString().isBlank() &&
-                !edtSignupName.text.toString().isBlank()) {
-                return true
-            }
-            return false
-        }
+    private fun idRegex(id : String) : Boolean{
+        return id.matches("^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{6,10}$".toRegex())
     }
 
-    private fun btnActivated(){
+    private fun pwRegex(pw : String) : Boolean{
+        return pw.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*?])[A-Za-z[0-9]!@#\$%^&*?]{6,12}$".toRegex())
+    }
+
+    private fun flagCheck(){
+        binding.btnSign.isEnabled = idFlag && pwFlag && nameFlag
+    }
+
+    private fun textWatcher(){
         with(binding){
-            edtSignupEmail.addTextChangedListener(object : TextWatcher{
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun afterTextChanged(p0: Editable?) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    btnSign.isEnabled = isEmpty()
+            edtSignupId.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    if(idRegex(edtSignupId.text.toString())){
+                        textinputlayoutId.error = null
+                        idFlag = true
+                    }
+                    else{
+                        textinputlayoutId.error = "아이디 형식이 올바르지 않습니다."
+                    }
+                    flagCheck()
                 }
             })
+
             edtSignupPw.addTextChangedListener(object : TextWatcher{
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun afterTextChanged(p0: Editable?) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    btnSign.isEnabled = isEmpty()
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    if(pwRegex(edtSignupPw.text.toString())){
+                        textinputlayoutPw.error = null
+                        pwFlag = true
+                    }
+                    else{
+                        textinputlayoutPw.error = "비밀번호 형식이 올바르지 않습니다."
+                    }
+                    flagCheck()
                 }
             })
 
             edtSignupName.addTextChangedListener(object : TextWatcher{
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun afterTextChanged(p0: Editable?) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    btnSign.isEnabled = isEmpty()
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    if(edtSignupName.text.toString().isNotEmpty()){
+                        textinputlayoutName.error = null
+                        nameFlag = true
+                    }
+                    else{
+                        textinputlayoutName.error = "이름을 작성해주세요."
+                    }
+                    flagCheck()
                 }
             })
         }
     }
 
     private fun clickEvent(){
-        with(binding){
-            btnSign.setOnClickListener{
-                signupService.signUp(
-                    RequestSignUp(
-                        binding.edtSignupEmail.text.toString(),
-                        binding.edtSignupPw.text.toString(),
-                        binding.edtSignupName.text.toString()
-                    )
-                ).enqueue(object : Callback <ResponseSignUp> {
-                    override fun onResponse(
-                        call: Call<ResponseSignUp>,
-                        response: Response<ResponseSignUp>
-                    ) {
-                        if(response.isSuccessful){
-                            Toast.makeText(this@SignUpActivity, "회원가입 성공", Toast.LENGTH_SHORT).show()
-                            startActivity(Intent(this@SignUpActivity, SignInActivity::class.java)) //화면전환
-                            finish()
-                        }
-                        else{
-                            Toast.makeText(this@SignUpActivity,"회원가입 실패",Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                    override fun onFailure(call: Call<ResponseSignUp>, t: Throwable) {
-                        Toast.makeText(this@SignUpActivity,"서버오류",Toast.LENGTH_SHORT).show()
-                    }
-                })
-            }
+        binding.btnSign.setOnClickListener{
+            viewModel.signUp(
+                binding.edtSignupId.text.toString(),
+                binding.edtSignupPw.text.toString(),
+                binding.edtSignupName.text.toString()
+            )
+        }
+        viewModel.signupResult.observe(this){
+            Toast.makeText(this@SignUpActivity, "회원가입 성공", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@SignUpActivity, SignInActivity::class.java)) //화면전환
+            finish()
         }
     }
 }
